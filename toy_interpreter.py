2e72fc98955b1de817aa965506107a003aa9bc1a
unknown <jasonwhister@gmail.com>
2018-05-25 23:55:40 +0800

    Add calc

--- toy_interpreter.py
+++ toy_interpreter.py
@@ -43,6 +43,58 @@ class Apply(object):
             r /= self.apply(e)
         return r
 
+    def calc_common(self, l, t):
+        for i, e in enumerate(l):
+            # print('i, e', i, e)
+            if e in t:
+                # print('if in')
+                token = self.apply([e, l[i - 1], l[i + 1]])
+                r = l[:i - 1] + [token] + l[i + 2:]
+                # print('r', r)
+                return r
+
+    def calc_list(self, l):
+        for i, e in enumerate(l):
+            # print('i, e', i, e)
+            if type(e) == list:
+                t = self.calc(['calc', e])
+                r = l[:i] + [t] + l[i + 1:]
+                return r
+
+    def calc_apply(self, l):
+        for i, e in enumerate(l):
+            if type(e) == list:
+                r = self.calc_list(l)
+                return r
+        if ('*' in l) or ('/' in l):
+            r = self.calc_common(l, ['*', '/'])
+            return r
+        elif ('+' in l) or ('-' in l):
+            r = self.calc_common(l, ['+', '-'])
+            return r
+
+    def calc(self, l):
+        f = l[1]
+        if len(f) > 1:
+            t = self.calc_apply(f)
+            r = self.calc(['calc', t])
+        else:
+            r = f[0]
+        return r
+
+    def define_variable(self, l):
+        v = self.apply(l[2])
+        k = l[1]
+        r = {
+            k: v,
+        }
+        self.var.update(r)
+        # print(self.var)
+        return 'N/A'
+
+    def call_variable(self, name):
+        return self.var[name]
+
     def apply(self, l):
         ops = {
             '+': self.plus,
@@ -67,3 +119,11 @@ if __name__ == '__main__':
     print(a.apply(['/', 1, 2]))
 
     print(a.apply(['+', 1, ['-', 2, 3]]))
+
+    l1 = ['calc', [1, '+', 2, '-', 3]]
+    l2 = ['calc', [1, '+', 2, '*', 3, '/', 2]]
+    l3 = ['calc', [1, '+', 2, '*', [1, '+', [1, '+', 1]], '/', 2]]
+
+    print(Apply().calc(l1))
+    print(Apply().calc(l2))
+    print(Apply().calc(l3))
